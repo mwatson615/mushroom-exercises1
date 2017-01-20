34ab5eb502fa6b794f9501eef587fc80da1036bb
Michael Watson <m.watson.tn@gmail.com>
2017-01-20 11:17:14 -0600

    css updated and firebase json loading to controller

--- factories/mushroomFactory.js
+++ factories/mushroomFactory.js
@@ -5,7 +5,7 @@ app.factory('mushroomFactory', function($http) {
 		getList : () => {
 			return $http.get('https://mushroom-madness-exercise.firebaseio.com/.json')
 			.then(function(value) {
-				console.log(value.data)
+				// console.log(value.data.mushrooms)
 				return value.data.mushrooms
 			})
 		}
